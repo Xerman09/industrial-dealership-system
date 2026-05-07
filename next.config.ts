b276a93bb39f0358704c95ce17d78d59d384d6ee
Maki <Fierrez@users.noreply.github.com>
2026-05-07 10:31:12 +0800

    Retail Directory Module
    checking lint error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    allowedDevOrigins: ['msi-andrie', '100.119.3.44'],
+    allowedDevOrigins: ['msi-andrie', '100.119.3.44','100.114.249.96'],
 };
 
 export default nextConfig;
